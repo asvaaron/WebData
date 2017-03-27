862dbe2ee46bf5ca3ed9e11f7b01a178074129b9
asvaaron <asvaaron@gmail.com>
2017-03-26 19:22:39 -0600

    review attributes

--- Twitter/Search.py
+++ Twitter/Search.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env pyh
 import tweepy
 from tweepy import OAuthHandler
-
 import twitterstream
 
 
@@ -12,6 +11,9 @@ class Search:
         auth.set_access_token(twitterstream.access_token, twitterstream.access_secret)
         self.api = tweepy.API(auth)
         # entry point for most of the Twitter operations .
+        if (not self.api):
+            print ("Can't Authenticate")
+
 
 
     def print_status (self):
@@ -19,16 +21,31 @@ class Search:
             print(status.text)
 
 
-    def search_query (self, query, count =None, lang="en"):
+    def search_query (self, query, count =None, lang="en OR es"):
         if count is None or (count >1000):
             count = 1000
             # max tweets
-        return self.api.search(q=query,count=count, lang=lang, show_user=True)
-
+        search_results= self.api.search(q=query, count=count, lang=lang, show_user=True)
+        for search_result in search_results:
+            print search_result.text.encode('utf8')
+            print search_result.created_at
+            print search_result.author._json['screen_name']
+            print search_result.lang
+            #print search_results.favorited
+            print search_result.entities.get('urls')
+            print search_result.entities.get('hashtags')
+            print search_result.entities.get('country')
+            print search_result.favorite_count
+            # Indicates approximately how many times this Tweet has been liked by Twitter users.
 
     def seach_results(self, search_results):
         for search_result in search_results:
             print search_result.text.encode('utf8')
             print search_result.created_at
             print search_result.author._json['screen_name']
+            print search_result.lang
+            #print search_result.place._json['country']
+
+
+
 
--- Twitter/__init__.py
+++ Twitter/__init__.py
@@ -5,4 +5,4 @@ import Search
 if __name__ == "__main__":
     Search = Search.Search()
     #Search.print_status()
-    Search.search_results("soccer",100)
+    Search.search_query("beer cerveza",100)
